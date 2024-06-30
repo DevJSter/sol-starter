fecca68c197a9d325c607aa8432382e969595bf4
Shubham Tiwari <shubht3303@gmail.com>
2024-06-30 19:03:17 +0530

    chores: creating FundMe.sol and other contracts

---
+++ ExtraStorage.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.8.2 <0.9.0;
+
+import "./SimpleStorage.sol";
+
+contract ExtraStorage  is SimpleStorage{
+    // +5 to any number we give it
+    //override
+    // virtual override
+
+    function store(uint256 _favriteNumber) public  override {
+        favoriteNumber = _favriteNumber +5;
+
+    }
+    
+}
