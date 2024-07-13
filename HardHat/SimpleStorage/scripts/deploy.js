d7b938ef0252e9e4636f67126a43e04733a144b3
Shubham Tiwari <shubht3303@gmail.com>
2024-07-13 22:10:12 +0530

    deploy.js

--- HardHat/SimpleStorage/scripts/deploy.js
+++ HardHat/SimpleStorage/scripts/deploy.js
@@ -16,7 +16,7 @@ async function main() {
   if (network.config.chainId === 11155111 && process.env.ETHERSCAN_API_KEY) {
     console.log("Waiting for block confirmations...")
     // Not functionable in version 6^ ethers ----->
-    await simpleStorage.deploymentTransaction().wait(6)
+    await simpleStorage.deployed().wait(6)
     await verify(simpleStorage.target, [])
   }
 
