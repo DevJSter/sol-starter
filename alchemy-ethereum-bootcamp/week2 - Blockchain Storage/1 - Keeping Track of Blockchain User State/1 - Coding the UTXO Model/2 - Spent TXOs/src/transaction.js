dca820fab0ccadb471bd5dc98392e35db36a180b
Shubham Tiwari <shubht3303@gmail.com>
2024-09-06 19:11:07 +0530

    UTXo's Done

--- alchemy-ethereum-bootcamp/week2 - Blockchain Storage/1 - Keeping Track of Blockchain User State/1 - Coding the UTXO Model/2 - Spent TXOs/src/transaction.js
+++ alchemy-ethereum-bootcamp/week2 - Blockchain Storage/1 - Keeping Track of Blockchain User State/1 - Coding the UTXO Model/2 - Spent TXOs/src/transaction.js
@@ -1,10 +1,26 @@
 class Transaction {
     constructor(inputUTXOs, outputUTXOs) {
-        this.transaction = { inputUTXOs, outputUTXOs}
+        this.transaction = { inputUTXOs, outputUTXOs };
     }
+
     execute() {
-        const isDoubleSpend = this.transaction.inputUTXOs.findIndex(utxo => utxo.spent == true);
-        if (isDoubleSpend !== -1) throw new Error('exist transaction that already was spend');
+        // Check for double spend
+        const isDoubleSpend = this.transaction.inputUTXOs.findIndex(utxo => utxo.spent === true);
+        if (isDoubleSpend !== -1) {
+            throw new Error('Exist transaction that already was spent');
+        }
+
+        // Calculate total input and output values
+        const totalInputValue = this.transaction.inputUTXOs.reduce((acc, utxo) => acc + utxo.amount, 0);
+        const totalOutputValue = this.transaction.outputUTXOs.reduce((acc, utxo) => acc + utxo.amount, 0);
+
+        // Check for insufficient UTXO funds
+        if (totalInputValue < totalOutputValue) {
+            throw new Error('Insufficient UTXO funds');
+        }
+
+        // Mark input UTXOs as spent
+        this.transaction.inputUTXOs.forEach(utxo => utxo.spent = true);
     }
 }
 
