8c957ecd8485bee14f9d3af0eb55a0d983f968e1
Shubham Tiwari <shubht3303@gmail.com>
2024-09-20 23:58:41 +0530

    test scripts and deploy scripts for SimpleStorage

--- Foundry-/mock/script/SimpleStorage.s.sol
+++ Foundry-/mock/script/SimpleStorage.s.sol
@@ -2,4 +2,16 @@
 
 pragma solidity ^0.8.24;
 
-    
+import {Script, console} from "forge-std/Script.sol";
+
+import {SimpleStorage} from "../src/SimpleStorage.sol";
+
+contract SimpleStorageScript is Script {
+        function run() external returns(SimpleStorage) {
+                vm.startBroadcast();
+                SimpleStorage simpleStorage = new SimpleStorage();
+
+                vm.stopBroadcast();
+                return simpleStorage;
+        }
+}
